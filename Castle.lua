3b5b800d0acfc742bca4a87128faf42489603ddb
GhoulMadness <62052147+GhoulMadness@users.noreply.github.com>
2021-12-08 22:12:26 +0100

    Castle hotfix

--- Castle.lua
+++ Castle.lua
@@ -1,29 +1,31 @@
 ------------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------- Castle Table -----------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------
-gvCastle = gvCastle or {}
+	gvCastle = gvCastle or {}
 
--- Blockreichweite in Siedler-cm, abhängig von der Fläche der Map
-gvCastle.BlockRange = 4000 + math.floor(((Logic.WorldGetSize()/100)^2)/110)
--- max. Anzahl erlaubter Schlösser
-gvCastle.CastleLimit = 2
-gvCastle.HQIDTable = {}
-gvCastle.PositionTable = {}
-gvCastle.AmountOfCastles = {}
-if CNetwork then
-	for i = 1,XNetwork.GameInformation_GetMapMaximumNumberOfHumanPlayer() do 
-		gvCastle.AmountOfCastles[i] = 0
-		gvCastle.HQIDTable = {Logic.GetEntities(Entities.PB_Headquarters1,i)}
+	-- Blockreichweite in Siedler-cm, abhängig von der Fläche der Map
+	gvCastle.BlockRange = 4000 + math.floor(((Logic.WorldGetSize()/100)^2)/110)
+	-- max. Anzahl erlaubter Schlösser
+	gvCastle.CastleLimit = 2
+	gvCastle.HQIDTable = {}
+	gvCastle.PositionTable = {}
+	gvCastle.AmountOfCastles = {}
+	if CNetwork then
+		for i = 1,XNetwork.GameInformation_GetMapMaximumNumberOfHumanPlayer() do 
+			gvCastle.AmountOfCastles[i] = 0
+			gvCastle.HQIDTable = {Logic.GetEntities(Entities.PB_Headquarters1,i)}
+			table.remove(gvCastle.HQIDTable,1)
+			for k = 1,table.getn(gvCastle.HQIDTable) do
+				local posX, posY = Logic.GetEntityPosition(gvCastle.HQIDTable[k])
+				gvCastle.PositionTable[k]={X = posX, Y = posY}
+			end
+		end
+	else
+		gvCastle.AmountOfCastles[1] = 0
+		gvCastle.HQIDTable = {Logic.GetEntities(Entities.PB_Headquarters1,12)}
 		table.remove(gvCastle.HQIDTable,1)
 		for k = 1,table.getn(gvCastle.HQIDTable) do
-			gvCastle.PositionTable[k]={Logic.GetEntityPosition(gvCastle.HQIDTable[k])}
+			local posX,posY = Logic.GetEntityPosition(gvCastle.HQIDTable[k])
+			gvCastle.PositionTable[k]={X = posX, Y = posY}
 		end
 	end
-else
-	gvCastle.AmountOfCastles[1] = 0
-	gvCastle.HQIDTable = {Logic.GetEntities(Entities.PB_Headquarters1,12)}
-	table.remove(gvCastle.HQIDTable,1)
-	for k = 1,table.getn(gvCastle.HQIDTable) do
-		gvCastle.PositionTable[k]={Logic.GetEntityPosition(gvCastle.HQIDTable[k])}
-	end
-end
