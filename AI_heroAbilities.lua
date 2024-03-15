a63fc02d43e4806f4b26631ad253c4feef8585a0
GhoulMadness <62052147+GhoulMadness@users.noreply.github.com>
2024-03-15 16:03:32 +0100

    hero abilities update

--- AI_heroAbilities.lua
+++ AI_heroAbilities.lua
@@ -1,5 +1,11 @@
 gvHeroAbilities = {	UnitsTreshold = 10,
 					DefaultRange = 800,
+					InternalAbilities = {InitialCooldown = - 6000,
+										["Hero13"] = {	["StoneArmor"] = {LastTimeUsed = {}, Cooldown = gvHero13.Cooldown.StoneArmor},
+														["DivineJudgment"] = {LastTimeUsed = {}, Cooldown = gvHero13.Cooldown.DivineJudgment}},
+										["Hero14"] = {	["CallOfDarkness"] = {LastTimeUsed = {}, Cooldown = gvHero14.CallOfDarkness.Cooldown},
+														["LifestealAura"] = {LastTimeUsed = {}, Cooldown = gvHero14.LifestealAura.Cooldown},
+														["RisingEvil"] = {LastTimeUsed = {}, Cooldown = gvHero14.RisingEvil.Cooldown}}},
 					AbilitiesByHero = {	[Entities.PU_Hero1] = {Abilities.AbilityInflictFear},
 										[Entities.PU_Hero1a] = {Abilities.AbilityInflictFear},
 										[Entities.PU_Hero1b] = {Abilities.AbilityInflictFear},
@@ -19,6 +25,7 @@ gvHeroAbilities = {	UnitsTreshold = 10,
 										[Entities.PU_Hero14] = {"CallOfDarkness", "LifestealAura", "RisingEvil"}
 									},
 					CastAbility = {	[Abilities.AbilityInflictFear] = function(_heroID)
+																		Logic.GroupStand(_heroID);
 																		(SendEvent or CSendEvent).HeroInflictFear(_heroID)
 																	end,
 									[Abilities.AbilityPlaceBomb] = 	function(_heroID, _posX, _posY)
@@ -31,25 +38,62 @@ gvHeroAbilities = {	UnitsTreshold = 10,
 																			(SendEvent or CSendEvent).HeroActivateAura(_heroID)
 																		end,
 									[Abilities.AbilityCircularAttack] = function(_heroID)
+																			Logic.GroupStand(_heroID);
 																			(SendEvent or CSendEvent).HeroCircularAttack(_heroID)
 																		end,
 									[Abilities.AbilitySummon] = function(_heroID)
 																	(SendEvent or CSendEvent).HeroSummon(_heroID)
 																end,
 									[Abilities.AbilityConvertSettlers] = function(_heroID, _targetID)
+																			Logic.GroupStand(_heroID);
 																			(SendEvent or CSendEvent).HeroConvertSettler(_heroID, _targetID)
 																		end,
 									[Abilities.AbilityShuriken] = 	function(_heroID, _targetID)
+																		Logic.GroupStand(_heroID);
 																		(SendEvent or CSendEvent).HeroShuriken(_heroID, _targetID)
 																	end,
 									[Abilities.AbilitySniper] = function(_heroID, _targetID)
+																	Logic.GroupStand(_heroID);
 																	(SendEvent or CSendEvent).HeroSnipeSettler(_heroID, _targetID)
 																end,
-									["StoneArmor"] = function(_heroID) end,
-									["DivineJudgment"] = function(_heroID) end,
-									["CallOfDarkness"] = function(_heroID) gvHero14.CallOfDarkness.SpawnTroops(_heroID) end,
-									["LifestealAura"] = function(_heroID) end,
-									["RisingEvil"] = function(_heroID, _targetID) end
+									["StoneArmor"] = function(_heroID, _player)
+														local starttime = Logic.GetTimeMs()
+														gvHeroAbilities.InternalAbilities.Hero13.StoneArmor.LastTimeUsed[_player] = starttime/1000
+														if not gvHero13.TriggerIDs.StoneArmor.DamageStoring[_player] then
+															gvHero13.TriggerIDs.StoneArmor.DamageStoring[_player] = Trigger.RequestTrigger(Events.LOGIC_EVENT_ENTITY_HURT_ENTITY, nil, "Hero13_StoneArmor_StoreDamage", 1, nil, {_heroID, starttime})
+														end
+														if not gvHero13.TriggerIDs.StoneArmor.DamageApply[_player] then
+															gvHero13.TriggerIDs.StoneArmor.DamageApply[_player] = Trigger.RequestTrigger(Events.LOGIC_EVENT_EVERY_TURN, nil, "Hero13_StoneArmor_ApplyDamage", 1, nil, {_heroID, starttime})
+														end
+													end,
+									["DivineJudgment"] = function(_heroID, _player)
+															local starttime = Logic.GetTimeMs()
+															local basedmg = Logic.GetEntityDamage(_heroID)
+															local posX,posY = Logic.GetEntityPosition(_heroID)
+															gvHeroAbilities.InternalAbilities.Hero13.DivineJudgment.LastTimeUsed[_player] = starttime/1000
+															if not gvHero13.TriggerIDs.DivineJudgment.DMGBonus[_player] then
+																gvHero13.TriggerIDs.DivineJudgment.DMGBonus[_player] = Trigger.RequestTrigger(Events.LOGIC_EVENT_ENTITY_HURT_ENTITY, nil, "Hero13_DMGBonus_Trigger", 1, nil, {_heroID, starttime})
+																Logic.CreateEffect(GGL_Effects.FXKerberosFear, posX, posY)
+															end
+															if not gvHero13.TriggerIDs.DivineJudgment.Judgment[_playerID] then
+																gvHero13.TriggerIDs.DivineJudgment.Judgment[_playerID] = Trigger.RequestTrigger(Events.LOGIC_EVENT_EVERY_TURN, nil, "Hero13_DivineJudgment_Trigger", 1, nil, {_heroID,basedmg,posX,posY,starttime})
+															end
+														end,
+									["CallOfDarkness"] = function(_heroID, _player)
+															gvHero14.CallOfDarkness.SpawnTroops(_heroID)
+															gvHeroAbilities.InternalAbilities.Hero14.CallOfDarkness.LastTimeUsed[_player] = Logic.GetTime()
+														end,
+									["LifestealAura"] = function(_heroID, _player)
+															local starttime = Logic.GetTime()
+															gvHeroAbilities.InternalAbilities.Hero14.LifestealAura.LastTimeUsed[_player] = starttime;
+															(SendEvent or CSendEvent).HeroActivateAura(_heroID)
+															gvHero14.LifestealAura.TriggerIDs[player] = Trigger.RequestTrigger(Events.LOGIC_EVENT_ENTITY_HURT_ENTITY, nil, "Hero14_Lifesteal_Trigger", 1, nil, {_heroID, starttime})
+														end,
+									["RisingEvil"] = function(_heroID, _player)
+														Logic.GroupStand(_heroID)
+														gvHero14.RisingEvil.SpawnEvilTower(_heroID)
+														gvHeroAbilities.InternalAbilities.Hero14.RisingEvil.LastTimeUsed[_player] = Logic.GetTime()
+													end
 								},
 					CheckByAbility = {	[Abilities.AbilityInflictFear] = function(_heroID, _posX, _posY, _player)
 																			local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
@@ -59,16 +103,16 @@ gvHeroAbilities = {	UnitsTreshold = 10,
 																			return false
 																		end,
 										[Abilities.AbilityPlaceBomb] = 	function(_heroID, _posX, _posY, _player)
-																			local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
+																			local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange*2)
 																			if num >= gvHeroAbilities.UnitsTreshold then
-																				local postable = GetNodesInCircleAndRange({X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
+																				local postable = GetNodesInCircleAndRange({X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange*2)
 																				local pos = GetPositionClump(postable, 500, 100)
 																				return true, pos.X, pos.Y
 																			end
 																			return false
 																		end,
 										[Abilities.AbilityBuildCannon] = function(_heroID, _posX, _posY, _player)
-																			local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
+																			local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange*2)
 																			if num >= gvHeroAbilities.UnitsTreshold then
 																				return true, _posX, _posY
 																			end
@@ -97,7 +141,7 @@ gvHeroAbilities = {	UnitsTreshold = 10,
 																		return false
 																	end,
 										[Abilities.AbilityConvertSettlers] = function(_heroID, _posX, _posY, _player)
-																				local id = GetNearestEnemyInRange(_player, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange, true)
+																				local id = GetNearestEnemyInRange(_player, {X = _posX, Y = _posY}, 1400, true)
 																				if id then
 																					return true, id
 																				end
@@ -106,15 +150,15 @@ gvHeroAbilities = {	UnitsTreshold = 10,
 										[Abilities.AbilityShuriken] = 	function(_heroID, _posX, _posY, _player)
 																			local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
 																			if num >= gvHeroAbilities.UnitsTreshold then
-																				local id = GetNearestEnemyInRange(_player, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange, true)
+																				local id = GetNearestEnemyInRange(_player, {X = _posX, Y = _posY}, 2000, true)
 																				return true, id
 																			end
 																			return false
 																		end,
 										[Abilities.AbilitySniper] = function(_heroID, _posX, _posY, _player)
 																		local enemies = BS.GetAllEnemyPlayerIDs(_player)
-																		for eID in CEntityIterator.Iterator(CEntityIterator.OfAnyPlayerFilter(unpack(enemies)), CEntityIterator.OfCategoryFilter(EntityCategories.Hero),
-																		CEntityIterator.InCircleFilter(_posX, _posY, gvHeroAbilities.DefaultRange)) do
+																		for eID in CEntityIterator.Iterator(CEntityIterator.OfAnyPlayerFilter(unpack(enemies)), CEntityIterator.OfAnyCategoryFilter(EntityCategories.Hero, EntityCategories.Cannon),
+																		CEntityIterator.InCircleFilter(_posX, _posY, 3800)) do
 																			if Logic.IsEntityAlive(eID) then
 																				return true, eID
 																			end
@@ -122,19 +166,43 @@ gvHeroAbilities = {	UnitsTreshold = 10,
 																		return false
 																	end,
 										["StoneArmor"] = function(_heroID, _posX, _posY, _player)
+															if GetEntityHealth(_heroID) <= 20 then
+																local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
+																if num >= gvHeroAbilities.UnitsTreshold then
+																	return true
+																end
+															end
 															return false
 														end,
 										["DivineJudgment"] = function(_heroID, _posX, _posY, _player)
+																if GetEntityHealth(_heroID) <= 5
+																or (gvHero13.AbilityProperties.StoneArmor.DamageStored[_player]
+																and (gvHero13.AbilityProperties.StoneArmor.DamageStored[_player] * gvHero13.AbilityProperties.StoneArmor.DamageFactor >= Logic.GetEntityHealth(_heroID)))
+																then
+																	local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
+																	if num >= gvHeroAbilities.UnitsTreshold then
+																		return true
+																	end
+																end
 																return false
 															end,
 										["CallOfDarkness"] = function(_heroID, _posX, _posY, _player)
+																local num = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
+																if num >= gvHeroAbilities.UnitsTreshold then
+																	return true
+																end
 																return false
 															end,
 										["LifestealAura"] = function(_heroID, _posX, _posY, _player)
+																local numE = GetNumberOfEnemiesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange*2)
+																local numA = GetNumberOfAlliesInRange(_player, {EntityCategories.Leader, EntityCategories.Soldier}, {X = _posX, Y = _posY}, gvHeroAbilities.DefaultRange)
+																if numE >= gvHeroAbilities.UnitsTreshold and numA >= gvHeroAbilities.UnitsTreshold then
+																	return true
+																end
 																return false
 															end,
 										["RisingEvil"] = function(_heroID, _posX, _posY, _player)
-															return false
+															return ({Logic.GetPlayerEntitiesInArea(_player, Entities.PB_Tower2, _posX, _posY, gvHero14.RisingEvil.Range)})[1] >= 1
 														end
 									},
 					HeroAbilityControl = function(_heroID)
@@ -146,14 +214,26 @@ gvHeroAbilities = {	UnitsTreshold = 10,
 						local player = Logic.EntityGetPlayer(_heroID)
 						for i = 1,table.getn(gvHeroAbilities.AbilitiesByHero[htype]) do
 							local ability = gvHeroAbilities.AbilitiesByHero[htype][i]
-							if Logic.HeroGetAbiltityChargeSeconds(_heroID, ability) == Logic.HeroGetAbilityRechargeTime(_heroID, ability) then
-								local allowed, param1, param2 = gvHeroAbilities.CheckByAbility[ability](_heroID, posX, posY, player)
-								if allowed then
-									gvHeroAbilities.CastAbility[ability](_heroID, param1, param2)
-									return true
+							if type(ability) == "number" then
+								if Logic.HeroGetAbiltityChargeSeconds(_heroID, ability) == Logic.HeroGetAbilityRechargeTime(_heroID, ability) then
+									local allowed, param1, param2 = gvHeroAbilities.CheckByAbility[ability](_heroID, posX, posY, player)
+									if allowed then
+										gvHeroAbilities.CastAbility[ability](_heroID, param1, param2)
+										return true
+									end
 								end
 							else
-								return
+								local str = BS.GetTableStrByHeroType(htype)
+								local TimePassed = math.floor(Logic.GetTime() - (gvHeroAbilities.InternalAbilities[str][ability].LastTimeUsed[player]
+								or gvHeroAbilities.InternalAbilities.InitialCooldown))
+								local cooldown = gvHeroAbilities.InternalAbilities[str][ability].Cooldown
+								if TimePassed >= cooldown then
+									local allowed, param1, param2 = gvHeroAbilities.CheckByAbility[ability](_heroID, posX, posY, player)
+									if allowed then
+										gvHeroAbilities.CastAbility[ability](_heroID, player, param1, param2)
+										return true
+									end
+								end
 							end
 						end
 					end
