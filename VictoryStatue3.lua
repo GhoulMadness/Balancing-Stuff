5fd4e20ec4acc927d5026c51ff86baf81aee70b6
GhoulMadness <62052147+GhoulMadness@users.noreply.github.com>
2024-12-06 12:46:44 +0100

    .

--- VictoryStatue3.lua
+++
@@ -1,9 +0,0 @@
-gvVictoryStatue3 = gvVictoryStatue3 or {}
-
-gvVictoryStatue3.BaseValue = 0.5
-gvVictoryStatue3.DecreaseValue = 0.1
-gvVictoryStatue3.MinimumValue = 0.1
-gvVictoryStatue3.Amount = {}
-for i = 1,16 do
-	gvVictoryStatue3.Amount[i] = 0
-end
